6356400f70b9121823c8cfd64430570f89d3032b
Sarah Jimenez <57870970+sarahmarie23@users.noreply.github.com>
2022-06-03 21:51:57 -0700

    Create RunningSum.kt
    Trying out some Kotlin problems :)

---
+++ LeetCode/RunningSum.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun runningSum(nums: IntArray): IntArray {
+        for(i in 1..nums.size-1)
+            nums[i] = nums[i] + nums[i-1]
+        
+        return nums
+    }
+}
